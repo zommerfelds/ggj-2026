0d0320542e53e514567a32f9154accbfc66b22d2
Björn Carlin <bjorn@arktos.se>
2026-01-30 11:55:47 +0100

    Now with control input

--- test_scene.gd
+++ test_scene.gd
@@ -4,7 +4,12 @@ extends Node2D
 var direction = Vector2(2, 2)
 
 
-func _process(_delta: float) -> void:
+func _physics_process(_delta: float) -> void:
+	var controller = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")).limit_length(0.4)
+
+	direction = (direction + controller).limit_length(10.0)
 	$Label.position += direction
 	if $Label.position.x > get_viewport_rect().end.x - $Label.size.x:
 		direction.x = -abs(direction.x)
