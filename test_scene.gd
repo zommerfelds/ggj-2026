d3c13b16e971e9351ca20ec1db73a6bf88360f11
Björn Carlin <bjorn@arktos.se>
2026-01-31 11:24:38 +0100

    Rock has cube collision shape.

--- test_scene.gd
+++ test_scene.gd
@@ -7,9 +7,9 @@ var direction = Vector2(2, 2)
 func _physics_process(_delta: float) -> void:
 	var controller = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")).limit_length(0.4)
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")).limit_length(1.0)
 
-	direction = (direction + controller).limit_length(10.0)
+	direction = (direction + controller * 0.4).limit_length(10.0)
 	$Label.position += direction
 	if $Label.position.x > get_viewport_rect().end.x - $Label.size.x:
 		direction.x = -abs(direction.x)
