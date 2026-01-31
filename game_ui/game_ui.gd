50f590e9488c16d3df1c5937a42e747fc4601445
Valentin Schlattinger <v.schlattinger@gmail.com>
2026-01-31 20:46:28 +0100

    Add Paradox Void

--- game_ui/game_ui.gd
+++ game_ui/game_ui.gd
@@ -2,6 +2,7 @@ extends Node
 
 
 var level_preload = preload("res://level/level.tscn")
+var paradox_void = preload("res://level/paradox_void/paradox_void.tscn")
 
 @onready var level = $Level
 var level_index = 1
@@ -9,6 +10,7 @@ var level_index = 1
 
 func _ready() -> void:
 	SignalBus.connect("goal_reached", next_level)
+	SignalBus.connect("end_world", end_world)
 
 func _process(delta) -> void:
 	if (Input.is_action_pressed("skip_level_1") && Input.is_action_just_pressed("skip_level_2")
@@ -34,3 +36,16 @@ func next_level(delta: int = 1) -> void:
 	level = level_preload.instantiate()
 	level.level_index = level_index
 	add_child(level)
+
+func end_world(source: Vector3) -> void:
+	var paradox = paradox_void.instantiate()
+	paradox.position = source
+	level.add_child(paradox)
+	var targetSize = 3 * max(level.grid_size.x, level.grid_size.z)
+	var tween = get_tree().create_tween()
+	tween.tween_property(
+		paradox,
+		"scale",
+		Vector3(targetSize, targetSize, targetSize),
+		1.5
+	)
--- signal_bus.gd
+++ signal_bus.gd
@@ -6,3 +6,6 @@ signal goal_reached()
 
 # Whether the screen rotation is enabled.
 signal can_rotate(bool)
+
+# End the world. Param denotes the source of the end.
+signal end_world(Vector3)
