1585d662c00cbebb6a56cb422e6312b8df45a4cf
Björn Carlin <bjorn@arktos.se>
2026-01-31 15:23:49 +0100

    Add invisible walls at level bounds

--- level/level.gd
+++ level/level.gd
@@ -6,6 +6,7 @@ extends Node3D
 var rock_scene = preload("res://level/rock/rock.tscn")
 var plant_scene = preload("res://level/plant/plant.tscn")
 var goal_scene = preload("res://level/goal/goal.tscn")
+var player_scene = preload("res://Player/Player.tscn")
 
 
 # Only X and Z matter for the grid size
@@ -18,46 +19,63 @@ func _ready() -> void:
 			grid_size = Vector3i(4, -1, 4)
 			add_plant(3, 0)
 			add_goal(3, 3)
-			set_player(0, 0)
+			add_player(0, 0)
 		2:
 			grid_size = Vector3i(6, -1, 4)
 			add_rock(0, 3)
 			add_plant(5, 0)
 			add_goal(5, 3)
-			set_player(0, 0)
+			add_player(0, 0)
 		_:
 			grid_size = Vector3i(6, -1, 6)
 			add_rock(1, 3)
 			add_rock(3, 1)
 			add_plant(2, 2)
 			add_goal(1, 1)
-			set_player(4, 4)
+			add_player(4, 4)
 
 	# Resize floor:
 	$Floor/FloorMesh.mesh.size = Vector2(grid_size.x, grid_size.z)
-	$Floor/FloorMesh.position.x = grid_size.x / 2
-	$Floor/FloorMesh.position.z = grid_size.z / 2
+	$Floor/FloorMesh.position.x = grid_size.x / 2.0
+	$Floor/FloorMesh.position.z = grid_size.z / 2.0
 	$Floor/FloorMesh.get_surface_override_material(0).uv1_scale = Vector3(grid_size.x, grid_size.z, 1)
 	$CameraPivot.position = $Floor/FloorMesh.position
 
+	# Create wall collision shapes:
+	var walls = StaticBody3D.new()
+	var box_shape = BoxShape3D.new()
+	var prototype = CollisionShape3D.new()
+	box_shape.size = Vector3(grid_size.x, 1.0, grid_size.z)
+	prototype.shape = box_shape
+	prototype.position = Vector3(grid_size.x / 2.0, 0.5, grid_size.z / 2.0)
+	for i in [Vector3(-1, 0, 0), Vector3(1, 0, 0), Vector3(0, 0, -1), Vector3(0, 0, 1)]:
+		var wall = prototype.duplicate()
+		wall.position += i * wall.shape.size
+		walls.add_child(wall)
+	add_child(walls)
+	prototype.free()
 
-func set_player(x, z) -> void:
-	$Objects/Player.position = Vector3(x + 0.5, 0, z + 0.5)
+
+func add_player(x, z) -> void:
+	var player = player_scene.instantiate()
+	player.position = Vector3(x + 0.5, 0.0, z + 0.5)
+	player.camera = $CameraPivot/Camera3D
+	$Objects.add_child(player)
 
 
 func add_rock(x, z) -> void:
 	var rock = rock_scene.instantiate()
-	$Objects.add_child(rock)
 	rock.position = Vector3(x + 0.5, 0, z + 0.5)
+	$Objects.add_child(rock)
 
 
 func add_plant(x, z) -> void:
 	var plant = plant_scene.instantiate()
-	$Objects.add_child(plant)
 	plant.position = Vector3(x + 0.5, 0, z + 0.5)
+	$Objects.add_child(plant)
 
 
 func add_goal(x, z) -> void:
 	var goal = goal_scene.instantiate()
-	$Objects.add_child(goal)
 	goal.position = Vector3(x + 0.5, 0, z + 0.5)
+	$Objects.add_child(goal)
