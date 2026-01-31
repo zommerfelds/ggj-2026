ee837ee190a5804647edf37d801d377375c5c470
Valentin Schlattinger <v.schlattinger@gmail.com>
2026-01-31 17:06:17 +0100

    Press "R" to reset level

--- game_ui/game_ui.gd
+++ game_ui/game_ui.gd
@@ -14,6 +14,14 @@ func _process(delta) -> void:
 	if (Input.is_action_pressed("skip_level_1") && Input.is_action_just_pressed("skip_level_2")
 	 || Input.is_action_pressed("skip_level_2") && Input.is_action_just_pressed("skip_level_1")):
 		next_level()
+	if (Input.is_action_just_pressed("reset_level")):
+		reset_level()
+
+func reset_level() -> void:
+	level.queue_free()
+	level = level_preload.instantiate()
+	level.level_index = level_index
+	add_child(level)
 
 func next_level() -> void:
 	level.queue_free()
