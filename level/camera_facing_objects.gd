273decf42ac3ad8fe9f5560555687c90688660f9
Valentin Schlattinger <v.schlattinger@gmail.com>
2026-01-31 15:38:29 +0100

    Implement masking behavior for camera-facing objects (except player)

--- level/camera_facing_objects.gd
+++ level/camera_facing_objects.gd
@@ -1,6 +1,8 @@
-extends Node
+extends Node3D
 
 @export var cameraPivot: Node3D
+
+const RAY_LENGTH = 10000
 
 func _process(delta: float) -> void:
 	var cameraPivotRotationY = cameraPivot.rotation.y
@@ -9,3 +11,18 @@ func _process(delta: float) -> void:
 			var mesh = child.get_node_or_null("Face")
 			if (mesh is Node3D):
 				mesh.rotation.y = cameraPivotRotationY
+
+func _physics_process(delta: float) -> void:
+	for child in get_children():
+		if (child is Node3D && !(child is Player)):
+			var isObjectMasked = checkIfObjectIsMasked(child)
+			child.process_mode = Node.PROCESS_MODE_DISABLED if isObjectMasked else Node.PROCESS_MODE_INHERIT
+#
+func checkIfObjectIsMasked(object: Node3D) -> bool:
+	var space_state = object.get_world_3d().direct_space_state
+	var directionToCamera = Vector3(0,0.577,1).rotated(Vector3(0,1,0), cameraPivot.rotation.y)
+	var target = object.global_position + directionToCamera * RAY_LENGTH
+	var query = PhysicsRayQueryParameters3D.create(object.global_position, target, 2)
+	query.collide_with_areas = true
+	var result = space_state.intersect_ray(query)
+	return result.get("collider") != null
