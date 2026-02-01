069fdd60c350abbc73f989183ec9d2b4da27a4d4
Yue <yue.li.dev@gmail.com>
2026-02-01 15:05:30 +0100

    bug fix only the one stands on should glow up
    bug fix only the one stands on should glow up

--- level/rotation_switch/rotation_switch.gd
+++ level/rotation_switch/rotation_switch.gd
@@ -3,6 +3,10 @@ extends Area3D
 @onready var mesh: MeshInstance3D = $MeshInstance3D
 @onready var mat: StandardMaterial3D = mesh.get_surface_override_material(0) as StandardMaterial3D
 
+func _ready():
+	mat = mesh.get_surface_override_material(0).duplicate() as StandardMaterial3D
+	mesh.set_surface_override_material(0, mat)
+
 func _on_body_entered(body: Node3D) -> void:
 	if body is Player:
 		SignalBus.can_rotate.emit(true)
