67496d15afd035e0b55b8ab55a1d3a47c1a2e9a2
Valentin Schlattinger <v.schlattinger@gmail.com>
2026-02-01 10:14:50 +0100

    More level names

--- level/level.gd
+++ level/level.gd
@@ -43,7 +43,7 @@ func _ready() -> void:
 			add_bush(2, 0)
 			add_player(4, 2)
 		3:
-			level_name = "Level 3: Potted plants can also be pushed"
+			level_name = "Level 3: Potted plants can be pushed, too"
 			grid_size = Vector3i(5, -1, 5)
 			add_goal(0, 0)
 			add_tall_bush(0, 2)
@@ -53,19 +53,21 @@ func _ready() -> void:
 			add_tall_bush(4, 2)
 			add_player(2, 4)
 		4:
-			level_name = "Level 4: Something is missing?"
+			level_name = "Level 4: Is something hiding here?"
 			grid_size = Vector3i(5, -1, 5)
 			add_goal(1, 2)
 			add_plant(2, 3)
 			add_plant(4, 2)
 			add_player(3, 1)
 		5:
+			level_name = "Level 5: Perspective matters"
 			grid_size = Vector3i(5, -1, 5)
 			add_goal(1, 1)
 			add_tall_bush(2, 2)
 			add_player(4, 1)
 			add_rotation_switch(4, 2)
 		6:
+			level_name = "Level 6: What if I can't see it?"
 			grid_size = Vector3i(6, -1, 4)
 			add_goal(0, 0)
 			for y in 6:
