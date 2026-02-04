cadb6ddb791f3bf4919cc538e316ebb4c98a992d
Björn Carlin <bjorn@arktos.se>
2026-02-04 14:23:43 +0100

    Reimport more pngs as compressed; update icon; gray loading screen background.

--- level/goal/goal.gd
+++ level/goal/goal.gd
@@ -5,7 +5,7 @@ class_name Goal
 func enable_flag():
 	%meshActive.visible = true
 	%meshDisabled.visible = false
-	
+
 func disable_flag():
 	%meshActive.visible = false
 	%meshDisabled.visible = true
