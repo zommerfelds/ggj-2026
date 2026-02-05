f831a2f136a2d36ac95ffae86bc907e35bd9b937
zommerfelds <christian.zommerfelds@gmail.com>
2026-02-05 09:06:55 +0100

    Smooth camera rotation

--- level/camera_pivot.gd
+++ level/camera_pivot.gd
@@ -1,50 +1,40 @@
 extends Node3D
 
 var targetRotationY = 45
-var rotationSpeed = 90
 
 var can_rotate = false
-var is_rotating = false
+var tween: Tween
 
 func _init() -> void:
 	SignalBus.connect("can_rotate", set_can_rotate)
 
 
 func _process(delta: float) -> void:
-	if !is_rotating:
-		if ((Input.is_action_just_pressed("rotate_right") || Input.is_action_just_pressed("touch_button")) and can_rotate):
+	if can_rotate and tween == null:
+		if Input.is_action_just_pressed("rotate_right") || Input.is_action_just_pressed("touch_button"):
 			SignalBus.camera_rotated.emit()
 			%AudioStreamPlayer.play()
 			targetRotationY += 90
-			if (targetRotationY > 180):
-				targetRotationY -= 360
-		if (Input.is_action_just_pressed("rotate_left") and can_rotate):
+			_start_rotation()
+		if Input.is_action_just_pressed("rotate_left"):
 			SignalBus.camera_rotated.emit()
 			%AudioStreamPlayer.play()
 			targetRotationY -= 90
-			if (targetRotationY < -180):
-				targetRotationY += 360
-	var rotationDiff = targetRotationY - rotation_degrees.y
-	var is_now_rotating = rotationDiff != 0
-	if is_now_rotating != is_rotating:
-		is_rotating = is_now_rotating
-		SignalBus.is_camera_rotating.emit(is_rotating)
-	if (rotationDiff < -180):
-		rotationDiff += 360
-	if (rotationDiff > 180):
-		rotationDiff -= 360
-	if rotationDiff > 0:
-		var step = delta * rotationSpeed
-		if (step > rotationDiff):
-			rotation_degrees.y = targetRotationY
-		else:
-			rotate_y(deg_to_rad(step))
-	else:
-		var step = -delta * rotationSpeed
-		if (step < rotationDiff):
-			rotation_degrees.y = targetRotationY
-		else:
-			rotate_y(deg_to_rad(step))
+			_start_rotation()
+		targetRotationY = wrapf(targetRotationY, -180, 180)
+
+
+func _start_rotation() -> void:
+	SignalBus.is_camera_rotating.emit(true)
+	var rotationDiff = wrapf(targetRotationY - rotation_degrees.y, -180, 180)
+	tween = get_tree().create_tween()
+	tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS) # For some reason this is needed to make the camera not twitching at the beginning and end
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(self, "rotation_degrees:y", rotation_degrees.y + rotationDiff, 1.0)
+	await tween.finished
+	tween = null
+	SignalBus.is_camera_rotating.emit(false)
+	
 
 
 func set_can_rotate(new_value: bool) -> void:
